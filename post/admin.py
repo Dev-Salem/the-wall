f974efba4fa9a294d68f5fc9dafa6b0902f8eef3
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-22 14:19:31 +0300

    feat: Add models for Post, Comment, and Vote
    This commit adds the necessary models for the Post, Comment, and Vote functionality. The models are defined in the `post/models.py` file and include fields for the body, author, created_at, and relationships between the models. The models are subclasses of the abstract `UserGeneratedContent` model, which provides common fields and behavior. Also register models on admin site with inline models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import *
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class VoteInline(admin.TabularInline):
+    model = Vote
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline, VoteInline]
+
+
 # Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(Vote)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,68 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models import Q
+
+
+class UserGeneratedContent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    body = models.CharField(
+        max_length=300,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+    class Meta:
+        abstract = True
+
+
+class Post(UserGeneratedContent):
+    pass
+
+
+class Comment(UserGeneratedContent):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+
+class Vote(models.Model):
+    VOTE_TYPE = (
+        ("up", "Upvote"),
+        ("down", "Downvote"),
+    )
+    vote = models.CharField(max_length=4, choices=VOTE_TYPE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["author", "post"],
+                name="each user can vote each post just once",
+            ),
+            models.UniqueConstraint(
+                fields=["author", "comment"],
+                name="each user can vote each comment just once",
+            ),
+            models.CheckConstraint(
+                name="Either post is null or comment is null",
+                check=Q(post__isnull=True, comment__isnull=False)
+                | Q(post__isnull=False, comment__isnull=True),
+            ),
+        ]
+
+    def __str__(self):
+        if self.post:
+            return f"{self.vote} on '{self.post}'"
+        else:
+            return f"{self.vote} on '{self.comment}'"
