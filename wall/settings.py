c417af18361fe3fdfe7eb4bbc86410acc98a9590
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-23 22:54:14 +0300

    w2

--- wall/settings.py
+++ wall/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False) == "True"
+DEBUG = os.getenv("DJANGO_DEBUG", False) == "True"
 
 
 ALLOWED_HOSTS = ["the-wall-418k.onrender.com", "localhost"]
