4e23cfd474e92f5e0d9b3fe63412647408aa8d00
Dev Salem <dev.salemsaleh@gmail.com>
2024-06-23 16:16:01 +0300

    feat: :rocket: Deploy static images (profile images) to S3

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    profile_image = models.ImageField(default="anon-profile.jpg", upload_to="")
+    profile_image = models.ImageField(default="anon-profile.jpg", upload_to="profiles/")
 
     def __str__(self):
         return self.username
--- wall/settings.py
+++ wall/settings.py
@@ -68,10 +68,11 @@ REST_AUTH = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "allauth.account.middleware.AccountMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -187,3 +188,11 @@ LOGGING = {
         "level": "WARNING",
     },
 }
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "eu-north-1"
